72c4efeb3be71f4b39b1d10adbe84766f994ab98
H-Hanif <302732920@student.rocmondriaan.nl>
2024-03-19 15:08:50 +0100

    paginas laten wrken

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,5 +1,12 @@
 const profile = document.querySelector(".profile")
+const portofolio = document.querySelector(".Portofolio")
 
 profile.addEventListener("click", () => {
     window.location.href = "/profile/profile.html";
+});
+
+
+
+portofolio.addEventListener("click", () => {
+    window.location.href = "/portofolio/portofolio.html";
 });
